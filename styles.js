5475c931e5c06d128d03f877d5df0218434a2a65
jpswt <joel_swt@yahoo.com>
2022-02-04 10:45:57 -0600

    fix populate todos

--- styles.js
+++ styles.js
@@ -1,18 +1,18 @@
     // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autes",
-        "completed": false
-    }]
+    // let arrayOfTodos = [
+    //     {
+    //     "userId": 14,
+    //     "id": 1,
+    //     "title": "delectus aut autem",
+    //     "completed": false
+    // },
+    // {
+    //     "userId": 20,
+    //     "id": 2,
+    //     "title": "delectus aut autes",
+    //     "completed": false
+    // }]
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -25,12 +25,12 @@
     }
     
     const populateTodos = () => {
-        const thingsTodo = document.getElementById("todo-list")
+
         for (i=0; i < arrayOfTodos.length; i++) {
-            const listItem = document.createElement("li")
+            let ol = document.getElementById("todo-list")
+            let li = document.createElement("li")
             const text = document.createTextNode(arrayOfTodos[i].title)
-            thingsTodo.appendChild(text)
-            thingsTodo.append(document.createElement("br"))
+            li.appendChild(text)
+            ol.appendChild(li)
         }
-    
     }
