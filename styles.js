5664f1a59e7001284d40c9a4e5317f1cb1e0018e
jpswt <joel_swt@yahoo.com>
2022-02-04 10:49:12 -0600

    first commit

--- styles.js
+++ styles.js
@@ -1,18 +1,18 @@
     // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autes",
-        "completed": false
-    }]
+    // let arrayOfTodos = [
+    //     {
+    //     "userId": 14,
+    //     "id": 1,
+    //     "title": "delectus aut autem",
+    //     "completed": false
+    // },
+    // {
+    //     "userId": 20,
+    //     "id": 2,
+    //     "title": "delectus aut autes",
+    //     "completed": false
+    // }]
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -25,12 +25,60 @@
     }
     
     const populateTodos = () => {
-        const thingsTodo = document.getElementById("todo-list")
+
         for (i=0; i < arrayOfTodos.length; i++) {
-            const listItem = document.createElement("li")
+            let ol = document.getElementById("todo-list")
+            let li = document.createElement("li")
             const text = document.createTextNode(arrayOfTodos[i].title)
-            thingsTodo.appendChild(text)
-            thingsTodo.append(document.createElement("br"))
+            li.appendChild(text)
+            ol.appendChild(li)
         }
-    
     }
+
+    
+    const filterTodos = () => {
+        const userNum = document.getElementById("userNum").value
+        let filterArray = arrayOfTodos.filter(filter => filter.userId == userNum)
+        clearAll()
+        populatefilteredTodos(filterArray)
+        console.log(filterArray)
+        
+        
+    }
+    
+    
+    const populatefilteredTodos = (filterArray) => {
+        for (i=0; i < filterArray.length; i++) {
+                let ol = document.getElementById("todo-list")            
+                let li = document.createElement("li")
+                const text = document.createTextNode(filterArray[i].title)
+                li.appendChild(text)
+                ol.appendChild(li)
+                
+        }   
+    }
+        
+    const storage = []
+    
+    const completedTodos = (filterArray) => {
+        clearAll()
+        for (i=0; i < filterArray.length; i++) {
+            if(filterArray[i].completed == true){
+                let ol = document.getElementById("todo-list")            
+                let li = document.createElement("li")
+                const text = document.createTextNode(filterArray[i].title)
+                li.appendChild(text)
+                ol.appendChild(li)
+            }
+        }   
+    }
+
+
+
+
+    const clearAll = () => {
+        let clear = document.getElementById("todo-list")
+        clear.textContent = ''
+        
+ }
+
